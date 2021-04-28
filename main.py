71b64f992832f4bee6d9a923dd0ca66d1eae245d
Piotr Durniat <piotr@durniat.com>
2021-04-28 23:02:04 +0200

    feat: add cyclic code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from codes.hammming_code import HammingCode
+from codes.cyclic_code import CyclicCode
+from codes.hamming_code import HammingCode
 from codes.repetition_code import RepetitionCode
 from utilities.data_generator import DataGenerator
 
+cyclic_code = CyclicCode(4, [1, 0, 1, 1], 0)
 hamming_code = HammingCode()
 repetitition_code = RepetitionCode(3)
 data_generator = DataGenerator()
@@ -17,3 +19,9 @@ print("Encoded with Repetition code: ", encoded_data_repetition)
 
 decoded_data_repetition = repetitition_code.decode(encoded_data_repetition)
 print("Decoded with Repetition code: ", decoded_data_repetition)
+
+encoded_data_cyclic = cyclic_code.encode(data)
+print("Encoded with Cyclic code: ", encoded_data_cyclic)
+
+decoded_data_cyclic = cyclic_code.decode(encoded_data_cyclic)
+print("Decoded with Cyclic code: ", decoded_data_cyclic)
