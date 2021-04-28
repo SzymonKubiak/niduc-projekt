6f9c330a025a17a81c2310e276eef41fe13dc5c2
Piotr Durniat <piotr@durniat.com>
2021-04-28 23:46:27 +0200

    feat: added trasmittion function through noisy channel

---
+++ utilities/noisy_channel.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class NoisyChannel():
+    def __init__(self, error_probability):
+        # Probability of an error occurring for each sent bit
+        self.error_probability = error_probability
+
+    def transmit_data(self, data_vector):
+        for i in range(len(data_vector)):
+            bit = data_vector[i]
+            if np.random.ranf() <= self.error_probability:
+                data_vector[i] = 0 if bit == 1 else 1
+
+        return data_vector
